32c691a44439174cd22302f9a6d1f8830035e16c
Kacper Jarocki <dev.kacperjarocki@proton.me>
2025-04-16 13:33:14 +0200

    initial backend

---
+++ backend/src/test/java/com/app/WalletTrack/WalletTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.WalletTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
